8ada3e8955c504e61bcf2c88f706a55a69ca2deb
Timothy Marias <timothyjamesmarias@gmail.com>
2023-04-23 17:05:35 -0700

    wip

--- cli.c
+++ cli.c
@@ -1,27 +1,25 @@
 #include "cli.h"
+#include "db.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 
 void parse_input() {
-    switch(input) {
+    char *token = strtok(input, " ");
+    switch(token) {
         case "create":
-            printf("create\n");
-            break;
-        case "drop":
-            printf("drop\n");
-            break;
-        case "use":
-            printf("use\n");
-            break;
-        case "show":
-            printf("show\n");
-            break;
-        case "quit":
-            printf("quit\n");
-            break;
-        default:
-            printf("Invalid command\n");
+            token = strtok(NULL, " ");
+            if (strcmp(token, "database") == 0) {
+                token = strtok(NULL, " ");
+                create_db(token);
+            }
+            else if (strcmp(token, "table") == 0) {
+                token = strtok(NULL, " ");
+                create_table(token);
+            }
+            else {
+                fprintf(stderr, "Invalid syntax\n");
+            }
             break;
     }
 }
--- cli.h
+++ cli.h
@@ -2,9 +2,12 @@
 #define CLI_H_
 #include <stdbool.h>
 
-#define MAX_INPUT_LENGTH 100
+#define MAX_INPUT_LENGTH 2048
+#define MAX_COMMAND_NUMBER 10
+#define MAX_COMMAND_LENGTH 20
 
 char input[MAX_INPUT_LENGTH];
+char commands[10][20];
 bool Q_FLAG = true;
 
 void get_input();
