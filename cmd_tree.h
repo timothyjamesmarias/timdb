45cbab3ef0d1d62a8a4094639c1c64ee3c430102
Timothy Marias <timothyjamesmarias@gmail.com>
2023-04-29 12:06:17 -0700

    added more functions

--- cmd_tree.h
+++ cmd_tree.h
@@ -2,8 +2,10 @@
 #define CMD_TREE_H_
 
 const char ** cmds = ["SHOW", "CREATE", "DELETE", "UPDATE", "INSERT"];
-const char ** keywords = ["DATABASE", "TABLE", "ROW", "COLUMN", "INTO"];
+const char ** keywords = ["INTO", "FROM", "WHERE", "VALUES", "SET", "ALL"];
+const char ** objects = ["DATABASE", "TABLE", "ROW", "COLUMN"];
 const char ** types = ["INT", "FLOAT", "STRING", "TEXT", "BOOL"];
+const char * variable = "[VARIABLE]";
 
 typedef struct CmdNode {
     char * cmd;
@@ -14,10 +16,11 @@ struct CmdNode * cmd_tree = NULL;
 
 struct CmdNode * new_node();
 void free_node(struct CmdNode * node);
+void free_tree(struct CmdNode * node);
+void print_tree(struct CmdNode * node);
 
 struct CmdNode * build_first_layer();
 struct CmdNode * build_tree();
-
 
 #endif
 
