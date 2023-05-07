4d079ce86bffb58b46eb9dc099a24ec54d152a8d
Timothy Marias <timothyjamesmarias@gmail.com>
2023-05-07 10:28:10 -0700

    removed parse tree files

--- timdb.h
+++ timdb.h
@@ -5,6 +5,12 @@
 #define MAX_INPUT_LENGTH 2048
 #define MAX_ARGS 512
 
+const char ** cmds = ["SHOW", "CREATE", "DELETE", "UPDATE", "INSERT"];
+const char ** predicates = ["INTO", "FROM", "WHERE", "VALUES", "SET", "ALL"];
+const char ** objects = ["DATABASE", "TABLE", "ROW", "COLUMN"];
+const char ** types = ["INT", "FLOAT", "STRING", "TEXT", "BOOL"];
+const char * variable = "X";
+
 char input[MAX_INPUT_LENGTH];
 char * cmds[MAX_ARGS];
 bool Q_FLAG = true;
